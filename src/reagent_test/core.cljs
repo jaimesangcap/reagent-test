1dd4193273984971fd1f8fb9717d804a475785af
Thomas Heller <th.heller@gmail.com>
2017-02-04 20:01:08 +0100

    media query

--- src/reagent_test/core.cljs
+++ src/reagent_test/core.cljs
@@ -1,10 +1,15 @@
 (ns reagent-test.core
-    (:require [reagent.core :as reagent]
-              [shadow.markup.css :as css :refer (defstyled)]))
+  (:require [reagent.core :as reagent]
+            [shadow.markup.css :as css :refer (defstyled)]))
+
+(def media-demo
+  "@media (max-width: 600px)")
 
 (defstyled h1 :h1
   [_]
-  {:color "red"})
+  {:color "red"
+   media-demo
+   {:color "green"}})
 
 ;; -------------------------
 ;; Views
